51e1f28fcd1b4f788fe70504fc48db3606748436
ishmael <ishmael@blackbalto.com>
2010-11-17 12:18:00 +0000

    added a combo type so that it doesn't have a default type, the hability to have javascript variables as data(the variable has to end in Var to be properly recognized), the hability of array in the x a y axis

--- lib/highchart.rb
+++ lib/highchart.rb
@@ -1,11 +1,11 @@
 require "highchart/hash"
 
 class Highchart
-  @@types = ['area', 'areaspline', 'bar', 'column', 'line', 'pie', 'scatter', 'spline']
+  @@types = ['area', 'areaspline', 'bar', 'column', 'line', 'pie', 'scatter', 'spline','combo']
   @@date_format = "<strong>%A</strong><br /> %m/%d/%Y"
   
 	attr_accessor :chart, :colors, :credits, :labels, :lang, :legend, :plotOptions, :point,
-	              :series, :subtitle, :symbols, :title, :toolbar, :tooltip, :x_axis, :y_axis
+	              :series, :subtitle, :symbols, :title, :toolbar, :tooltip, :x_axis, :y_axis,:exporting
 
   def self.method_missing(m, options={})
     format = options[:format] || 'jquery'
@@ -25,16 +25,7 @@ class Highchart
     @credits = {
       :enabled => false
     }
-    @legend = {
-      :layout => 'vertical', 
-      :style => {
-         :left => 'auto', 
-         :bottom => 'auto', 
-         :right => '50px', 
-         :top => '75px'
-      }
-    }
-    
+  
     # IE7 displays 'null' without this
     @title = {
       :text => ""
@@ -54,80 +45,40 @@ class Highchart
     case options[:type].to_s
     when 'area'
       @chart = {
-        :defaultSeriesType => 'area',
-        :margin => [50, 200, 60, 80]
+        :defaultSeriesType => 'area'
       }
     when 'areaspline'
       @chart = {
-        :defaultSeriesType => 'areaspline',
-        :margin => [50, 200, 60, 80]
+        :defaultSeriesType => 'areaspline'
       }
     when 'bar'
       @chart = {
         :inverted => true, 
         :defaultSeriesType => 'bar', 
-        :margin => [50, 50, 50, 100]
-      }
-      @plotOptions = {
-        :bar => {
-          :showInLegend => false,
-          :dataLabels => {
-            :y => -5,
-            :enabled => true, 
-            :color => '#666666'
-          }
-        }
       }
     when 'column'
       @chart = {
-        :defaultSeriesType => 'column', 
-        :margin => [50, 50, 50, 100]
-      }
-      @plotOptions = {
-        :column => {
-          :showInLegend => false
-        }
+        :defaultSeriesType => 'column'
       }
     when 'line'
       @chart = {
-        :defaultSeriesType => 'line',
-        :margin => [50, 200, 60, 80]
+        :defaultSeriesType => 'line'
       }
     when 'pie'
       @chart = {
-        :margin => [10, 10, 10, 10]
-      }
-      @plotOptions = {
-        :pie => {
-          :dataLabels => {
-            :enabled => true, 
-            :color => 'white', 
-            :style => {:font => '10pt Helvetica'}
-          }
-        }
+        :defaultSeriesType => 'pie'
       }
     when 'scatter'
       @chart = {
-        :defaultSeriesType => 'scatter',
-        :margin => [50, 200, 60, 80]
-      }
-      @plotOptions = {
-        :scatter => {
-          :marker => {
-            :radius => 5,
-            :states => {
-              :hover => {
-                :enabled => true,
-                :lineColor => '#666666'
-              }
-            }
-          }
-        }
+        :defaultSeriesType => 'scatter'
       }
     when 'spline'
       @chart = {
-        :defaultSeriesType => 'spline',
-        :margin => [50, 200, 60, 80]
+        :defaultSeriesType => 'spline'
+      }
+    when 'combo'
+      @chart = {
+
       }
     end
     
@@ -147,6 +98,12 @@ class Highchart
   end
   
   def jquery
+    "var chart = new Highcharts.Chart({ 
+      #{options} 
+    });"
+  end
+
+  def options
     query_params = instance_variables.sort.map do |var|
       case var.to_s
         
@@ -182,12 +139,13 @@ class Highchart
         set_x_axis
       when '@y_axis'
         set_y_axis
+      when '@exporting'
+        set_exporting
       end
     end.compact
 
-    "var chart = new Highcharts.Chart({ 
-      #{query_params.join(",\n")} 
-    });"
+    "#{query_params.join(",\n")}" 
+  
   end
 
   private
@@ -257,6 +215,7 @@ class Highchart
 			]"
     end
     
+
     def set_subtitle
       "subtitle: {
         #{concatenate_attributes(@subtitle)}
@@ -286,29 +245,60 @@ class Highchart
     end
     
     def set_x_axis   
+      if @x_axis.is_a?(Hash)
       "xAxis: {  
         #{concatenate_attributes(@x_axis)}
       }"
+      elsif @x_axis.is_a?(Array) 
+        "xAxis: [  
+          #{concatenate_attributes(@x_axis)}
+        ]"
+      end
     end
     
-    def set_y_axis   
-      "yAxis: {  
-        #{concatenate_attributes(@y_axis)}
-      }"
+    def set_y_axis  
+      if @x_axis.is_a?(Hash)
+          "yAxis: {  
+            #{concatenate_attributes(@y_axis)}
+          }"
+      elsif @x_axis.is_a?(Array) 
+          "yAxis: [  
+            #{concatenate_attributes(@y_axis)}
+          ]"
+      end 
+    end
+    
+    def set_exporting
+      "exporting: {
+      #{concatenate_attributes(@exporting)}
+			}"
     end
     
     # generic method that accepts a hash and concatenates its key/value pairs
     def concatenate_attributes(attr)
-      attrs = Array.new
-      attr.each do |key, value|
-        if value.is_a?(Hash)
-          attrs << "#{key}: {#{concatenate_attributes(value)}}"
-        else
-          attrs << "#{key}: #{typed_print(value)}"
-        end
-      end
+      if attr.is_a?(Hash)
+          attrs = Array.new
+          attr.each do |key, value|
+            if value.is_a?(Hash)
+              attrs << "#{key}: {#{concatenate_attributes(value)}}"
+            else          
+              attrs << "#{key}: #{typed_print(value)}"
+            end
+          end
       
-      attrs.join(",\n") + "\n"
+          attrs.join(",\n") + "\n"
+      elsif attr.is_a?(Array)
+        attrs = Array.new
+          attr.each do |series_hash|
+            ser = Array.new
+            series_hash.each do |key, value|
+                ser << "#{key}: #{typed_print(value)}"
+            end
+
+            attrs << "{#{ser.join(",\n")}}";
+          end
+          attrs.join(",\n") + "\n"
+      end
     end
     
     # method determines if value needs to be surrounded in single quotes
@@ -316,12 +306,12 @@ class Highchart
       if string.is_a?(Hash)
         values = Array.new
         string.each { |key, value| values << "#{key}: #{typed_print(value)}"}
-        "{" + values.join(", ") + "}"
+        "{ " + values.join(", ") + "}"
       elsif string.is_a?(Array)
         "[" + string.map { |e| typed_print(e) } .join(", ") + "]"
       elsif string.is_a?(Date)
         "'#{string.strftime(@@date_format)}'"
-      elsif string.is_a?(String) && string != 'null' && string.strip[0..7] != 'function'
+      elsif string.is_a?(String) && string != 'null' && string.index('function') == nil && string.match('Var') == nil
         "'" + string.gsub(/['"\\\x0]/,'\\\\\0') + "'"
       else
         string
